288f4fb81c6ab9bd3c2376b700d1e6d01464a718
Fabio Mendes <fabiojmendes@gmail.com>
2026-03-17 15:54:52 -0400

    Integrate Battle Board combat initialization and dice rolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import OBR from "@owlbear-rodeo/sdk";
+import OBR, { isImage } from "@owlbear-rodeo/sdk";
+import { Dice } from "dice-typescript";
 import { ID } from "./main.tsx";
 import StatBlock from "./StatBlock.tsx";
+import { getInitiativeBonus } from "./utils/helpers.ts";
 
 function App() {
-  const popover = () => {
+  const dice = new Dice();
+
+  const showStatblock = () => {
     OBR.popover.open({
       id: `${ID}/statblock`,
       url: "/statblock",
@@ -13,10 +17,47 @@ function App() {
       anchorOrigin: { horizontal: "RIGHT", vertical: "TOP" },
       marginThreshold: 75,
     });
+    OBR.action.close();
   };
 
-  const combat = () => {
-    console.log("Click");
+  const startCombat = () => {
+    OBR.scene.items.updateItems(
+      (item) => {
+        if (isImage(item)) {
+          const monster: any = item.metadata[`${ID}/monster`];
+          const battleBoard: any =
+            item.metadata["com.missing-link-dev.battle-board/metadata"];
+          return (
+            item.layer === "CHARACTER" &&
+            monster !== undefined &&
+            battleBoard?.inInitiative
+          );
+        } else {
+          return false;
+        }
+      },
+      (items) => {
+        for (const item of items) {
+          const monster: any = item.metadata[`${ID}/monster`];
+          const battleBoard: any =
+            item.metadata["com.missing-link-dev.battle-board/metadata"];
+          const initBonus = getInitiativeBonus(monster);
+          const formula = `1d20${initBonus >= 0 ? `+${initBonus}` : `${initBonus}`}`;
+          battleBoard.initiative = dice.roll(formula).total;
+
+          if (monster?.hp?.formula) {
+            const hp = dice.roll(monster.hp.formula).total;
+            battleBoard.maxHP = hp;
+            battleBoard.currentHP = hp;
+          }
+          battleBoard.ac = monster.ac[0].ac || monster.ac[0];
+        }
+        OBR.notification.show(
+          `BattleBoard updated with ${items.length} items!`,
+        );
+      },
+    );
+    OBR.action.close();
   };
 
   if (window.location.pathname === "/statblock") {
@@ -27,13 +68,13 @@ function App() {
     <section className="app-container">
       <h1>StatBlock</h1>
       <div className="button-group">
-        <button className="action-button" type="button" onClick={popover}>
+        <button className="action-button" type="button" onClick={showStatblock}>
           Show
         </button>
       </div>
       <div className="button-group">
-        <button className="action-button" type="button" onClick={combat}>
-          Combat!
+        <button className="action-button" type="button" onClick={startCombat}>
+          Start Combat!
         </button>
       </div>
     </section>
